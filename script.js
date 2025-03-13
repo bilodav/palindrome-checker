79eca85482bc34a0fef4815e13e3967d7597bf3f
bilodav <bilodavids@gmail.com>
2025-03-13 14:54:50 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+
+const checkButton = document.getElementById("check-btn");
+const resultDisplay =document.getElementById("result");
+
+const checkString = () =>{
+const inputText = document.getElementById("text-input").value;
+const regex = /[\s-),_.(-/]+/g ;
+const lowerString = inputText.toLowerCase();
+const finalString = lowerString.replace(regex, "");
+const arrayChars = finalString.split("");
+const arrayChars2 = [...arrayChars];
+const reverseChars = arrayChars2.reverse();
+const reverseString = reverseChars.join("");
+  if (finalString ===""){ 
+    alert("Please input a value")
+  }
+
+   else if (finalString === reverseString ){ 
+    return resultDisplay.innerHTML = `${inputText} is a palindrome!`
+  }
+  else{
+    return resultDisplay.innerHTML = `${inputText} is not a palindrome`
+  };
+
+
+
+};
